c953a3089a29a8206cd924caa88d9500abb5d750
Aiman khan <aimankhan4868@gmail.com>
2026-04-01 20:56:47 +0500

    Add files via upload

---
+++ chap43-48.js
@@ -0,0 +1,27 @@
+// q1
+function showAlert() {
+    alert("Welcome..");
+}
+
+// q2
+function showMessage(msg) {
+    alert("Thanks for purchasing: " + msg);
+}
+
+// q3
+function deleteRow(btn) {
+    var row = btn.parentNode.parentNode;
+    row.parentNode.removeChild(row);
+}
+// q5
+var num = 0;
+
+function increase() {
+    num++;
+    document.getElementById("num").innerText = num;
+}
+
+function decrease() {
+    num--;
+    document.getElementById("num").innerText = num;
+}
